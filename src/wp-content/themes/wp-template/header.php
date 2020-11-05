10be1b8e6d5a3405c015f25f8e0740c69e2fb6ea
KhoaVA <khoava@concrete-corp.com>
2020-11-05 16:59:11 +0700

    Implement top page

--- src/wp-content/themes/wp-template/header.php
+++ src/wp-content/themes/wp-template/header.php
@@ -11,26 +11,57 @@
 <body <?php body_class(); ?>>
     <div id="app" class="page-wrapper">
         <header class="header">
-            <div class="wrap-header container-full">
+            <div class="wrap-header">
                 <div class="header-logo">
                     <a href="<?php echo esc_url(home_url('/')); ?>">
                        <img src="<?php echo URL_IMAGE?>/common/logo.png" alt="">
                     </a>
-                </div><!-- .header-logo -->
-                <nav class="navMenu dropdown">
-                    <button class="c-open-menu dropdown-toggle" data-toggle="dropdown">Menu</button>
-                    <div class="dropdown-menu">
-                        <ul class='head-menu-items'>
-                            <?php
-                                wp_nav_menu(array(
-                                    'container' => '',
-                                    'items_wrap' => '%3$s',
-                                    'walker'        => new Fx_Walker_Nav_Menu()
-                                ));
-                            ?>
-                        </ul>
-                    </div>
-                </nav>
+                </div>
+                <div class='nav-header-wrapper'>
+                    <ul class='nav-header only-pc'>
+                        <li class='nav-header--item'>
+                            <a href='#'>
+                                News<br>
+                                <span>お知らせ</span>
+                            </a>
+                        </li>
+                         <li class='nav-header--item'>
+                            <a href='#'>
+                                Company<br>
+                                <span>私たちについて</span>
+                            </a>
+                          </li>
+                          <li class='nav-header--item'>
+                              <a href='#'>
+                                   Service<br>
+                                  <span>ソリューション/製品</span>
+                              </a>
+                          </li>
+                          <li class='nav-header--item'>
+                            <a href='#'>
+                                Case/Column<br>
+                                <span>事例とコラム</span>
+                            </a>
+                          </li>
+                          <li class='nav-header--item'>
+                            <a href='#'>
+                                Career<br>
+                                <span>採用情報</span>
+                            </a>
+                          </li>
+                          <li class='nav-header--item nav-header--item__last'>
+                            <a href='#'>
+                                 Contact<br>
+                                <span>お問い合わせ</span>
+                            </a>
+                          </li>
+                    </ul>
+
+                    <nav class="navMenu dropdown only-sp">
+                        <button class="btn-menu" onclick="closeNav()"><img src="<?php echo URL_IMAGE?>/icon/ic-menu.png" alt=""></button>
+                    </nav>
+
+                </div>
             </div>
         </header>
 
--- src/wp-content/themes/wp-template/page-template/page-top.php
+++ src/wp-content/themes/wp-template/page-template/page-top.php
@@ -5,7 +5,48 @@
 get_header();
 
 ?>
-  <main id="main-content" class="page-top">
-
+  <main id="main-content" class="page-top main-wrapper">
+     <div class='top-banner-wrapper'>
+         <img class='banner-img' src="<?php echo URL_IMAGE?>/common/mv.png" alt="banner">
+         <div class='banner-text'>
+            <h2 class='title'>テクノロジーと感動を<br>「かたち」に</h2>
+            <p class='sub-title'>スピードと飛躍的技術がもたらすイノベーションを鍵に<br>お客様の次世代を開く一歩を支援する企業となる。</p>
+         </div>
+         <div class='box-new container'>
+            <p class='news'>News</p>
+            <p class='date'>2020 04.30</p>
+            <p class='content'>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+         </div>
+     </div>
+     <div class='featured-wrapper'>
+        <h3 class='box-heading'>
+           Featured<br>
+           <span>特集</span>
+        </h3>
+        <div class='list-featured container'>
+            <div class='list-featured--item'>
+                <div class='featured-img-wrapper'>
+                   <img class='featured-img' src="<?php echo URL_IMAGE?>/featured/item-1.png" alt="featured-img">
+                </div>
+                <div class='featured-content'>
+                    <p class='title'>タイトルがはいりますタイトルがはいります。タイトルがはいります。</p>
+                    <p class='date'>2020 04.30</p>
+                </div>
+            </div>
+            <div class='list-featured--item'>
+                <div class='featured-img-wrapper'>
+                    <img class='featured-img' src="<?php echo URL_IMAGE?>/featured/item-2.png" alt="featured-img">
+                </div>
+                <div class='featured-content'>
+                    <p class='title'>タイトルがはいりますタイトルがはいります。タイトルがはいります。</p>
+                    <p class='date'>2020 04.30</p>
+                </div>
+            </div>
+        </div>
+        <div class='featured-page'>
+           <button class='btn-featured'><img class='ic-back' src="<?php echo URL_IMAGE?>/icon/back.png" alt="back"></button>
+           <button class='btn-featured'><img class='ic-next' src="<?php echo URL_IMAGE?>/icon/next.png" alt="next"></button>
+        </div>
+     </div>
   </main>
 <?php get_footer(); ?>
