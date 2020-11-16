e73602fb5445d54c45098008b4c40818b576f26a
KhoaVA <khoava@concrete-corp.com>
2020-11-16 18:40:07 +0700

    Implement service page

---
+++ src/wp-content/themes/wp-template/page-template/page-service.php
@@ -0,0 +1,145 @@
+<?php
+/*
+* Template Name: Service page
+*/
+get_header();
+
+?>
+  <main id="main-content" class="page-service main-wrapper">
+     <div class='service-banner'>
+         <div class='banner-content-wrapper container'>
+            <h1 class='banner-title'>
+                こころが動くARアプリ<br>
+                aug!オーグ
+            </h1>
+            <p class='banner-sub-title'>
+                画像入ります
+            </p>
+         </div>
+     </div>
+     <div class='service-main-wrapper'>
+         <div class='service--bg-img-top' style='background-image: url("<?php echo URL_IMAGE?>/service/bg-top.png")'>
+            <div class='service--bg-img-center' style='background-image: url("<?php echo URL_IMAGE?>/service/bg-center.png")'>
+                <div class='service-main--content-wrapper container'>
+                     <div class='service-main--application'>
+                        <div class='service-main--application--content'>
+                            <p class='service-main--application--content--title'>すでに100社様以上にお使いいただいているiOS/Android向け画像認識アプリです。</p>
+                            <p class='service-main--application--content--content'>aug!オーグをベースにオリジナルのコンテンツを開発すれば、動画で製品を紹介したり、リアルな3Dでシミュレーションさせたりと、様々なPR方法が可能となります。</p>
+                        </div>
+                        <div class='service-main--application-img-wrapper'>
+                            <img class='service-img-placeholder' src="<?php echo URL_IMAGE?>/service/service-img-placeholder.png" alt="service-img">
+                        </div>
+                     </div>
+                     <div class='service-main--point'>
+                        <div class='service-main--point--item'>
+                            <div class='service-main--point--item--header'>
+                                <div class='point-wrapper'>
+                                    <p class='point'>
+                                        Point <br>
+                                        <span>01</span>
+                                    </p>
+                                </div>
+                                <div class='point-line'>
+                                </div>
+                                <img src="<?php echo URL_IMAGE?>/service/L1.png" alt="service-img-L1">
+                                <div class='point-content'>
+                                    <p class='title'>かざしたところが動き出す</p>
+                                    <p class='content'>aug!オーグはかざす対象となる画像と一体となってコンテンツを表示します。あらかじめ登録したターゲット画像(マーカー)をトラッキングして(くっついて)コンテンツを表示することができます。</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class='service-main--point--item'>
+                            <div class='service-main--point--item--header'>
+                                <div class='point-wrapper'>
+                                    <p class='point'>
+                                        Point <br>
+                                        <span>02</span>
+                                    </p>
+                                </div>
+                                <div class='point-line'>
+                                </div>
+                                <img src="<?php echo URL_IMAGE?>/service/L2.png" alt="service-img-L2">
+                                <div class='point-content'>
+                                    <p class='title'>ターゲット画像を自動的に延長</p>
+                                    <p class='content'>aug!オーグはターゲット画像から外れてしまっても、コンテンツの表示を継続することができます。そして再びターゲット画像を発見すると、自然なかたちでトラッキングを開始します。</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class='service-main--point--item'>
+                            <div class='service-main--point--item--header'>
+                                <div class='point-wrapper'>
+                                    <p class='point'>
+                                        Point <br>
+                                        <span>03</span>
+                                    </p>
+                                </div>
+                                <div class='point-line'>
+                                </div>
+                                <img src="<?php echo URL_IMAGE?>/service/L3.png" alt="service-img-L3">
+                                <div class='point-content'>
+                                    <p class='title'>SNSシェア機能</p>
+                                    <p class='content'>aug!オーグではSNSシェア機能が基本機能として用意されています。Twitterでつぶやく、Facebook、Instagram、LINEに投稿するなど、驚きの写真をみんなと楽しむことができます。</p>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+                     <div class='service-main--video'>
+                         <div class='service-main--video-img-wrapper'>
+                             <img class='service-img-placeholder' src="<?php echo URL_IMAGE?>/service/service-img-placeholder.png" alt="service-img">
+                         </div>
+                         <div class='service-main--video--content'>
+                              <p class='service-main--video--content--title'>すでに100社様以上にお使いいただいているiOS/Android向け画像認識アプリです。</p>
+                              <p class='service-main--video--content--content'>aug!オーグをベースにオリジナルのコンテンツを開発すれば、動画で製品を紹介したり、リアルな3Dでシミュレーションさせたりと、様々なPR方法が可能となります。</p>
+                          </div>
+                     </div>
+                     <div class='read-more-wrapper'>
+                         <a href='#'>Read More</a>
+                         <img src="<?php echo URL_IMAGE?>/icon/btn-read-more.png"/>
+                     </div>
+                     <div class='service-list-color'>
+                        <div class='service-list-color--item'>
+                           <div class='service-list-color--item--img-wrapper'>
+                                <img class='service-img-placeholder' src="<?php echo URL_IMAGE?>/service/service-img-placeholder.png" alt="service-img">
+                           </div>
+                           <p class='service-list-color--item--title'>ぬってポン! 3DぬりえAR daubだーぶ</p>
+                           <p class='service-list-color--item--content'>色を塗ったぬりえが3Dになって動き出す、iOS/Android 向けARアプリです。 Daubだぶを元にしたオリジナルアプリの...続きを読む</p>
+                        </div>
+
+                        <div class='service-list-color--item'>
+                           <div class='service-list-color--item--img-wrapper'>
+                                <img class='service-img-placeholder' src="<?php echo URL_IMAGE?>/service/service-img-placeholder.png" alt="service-img">
+                           </div>
+                           <p class='service-list-color--item--title'>ぬってポン! 3DぬりえAR daubだーぶ</p>
+                           <p class='service-list-color--item--content'>色を塗ったぬりえが3Dになって動き出す、iOS/Android 向けARアプリです。 Daubだぶを元にしたオリジナルアプリの...続きを読む</p>
+                        </div>
+                     </div>
+                     <h2 class='service-title'>タイトル入りますタイトル入ります</h2>
+                     <div class='service-list-title'>
+                        <p class='service-list-title--item'><span></span>テキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入り</p>
+                        <p class='service-list-title--item'><span></span>テキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入り</p>
+                        <p class='service-list-title--item'><span></span>テキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入り</p>
+                        <p class='service-list-title--item'><span></span>テキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入り</p>
+                        <p class='service-list-title--item'><span></span>テキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入りますテキスト入り</p>
+                     </div>
+                      <h2 class='service-title'>事例紹介</h2>
+                      <div class='service-golgo-wrapper'>
+                          <div class='service-golgo--img-wrapper'>
+                            <img class='golgo-img-placeholder' src="<?php echo URL_IMAGE?>/service/golgo.png" alt="golgo-img">
+                          </div>
+                          <div class='service-golgo--content-wrapper'>
+                           <h2 class='title'>ゴルゴ13展</h2>
+                           <p class='content'>テキスト入ります。テキスト入ります。テキスト入ります。テキスト入ります。テキスト入ります。テキスト入ります。テキスト入ります。テキスト入ります。テキスト入ります。テキスト入ります。テキスト入ります。テキスト入ります。テキスト入ります。テキスト入ります。</p>
+                          </div>
+                      </div>
+                      <h2 class='service-title'>アプリ開発における当社の強み</h2>
+                 </div>
+            </div>
+
+
+         </div>
+      </div>
+
+  </main>
+<?php get_footer(); ?>
